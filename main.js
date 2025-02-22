d45c9aa426e42234c6f7892b2c22906ded7ee2d4
bhusha0919 <bhushangavhane06@gmail.com>
2025-02-22 11:19:52 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Frontend Developer", "Andriod Developer", "Web Developer"],
+    typeSpeed : 10,
+    backSpeed : 50,
+    backDelay : 1000,
+    loop : true
+});
