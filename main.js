104d8d9cf5d0c3fd46f877406e729779d4f3ed23
bhusha0919 <bhushangavhane06@gmail.com>
2025-02-22 15:40:11 +0530

    Responsive

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text",{
-    strings:["Frontend Developer", "Andriod Developer", "Web Developer"],
+    strings:["Frontend Developer", "Java Developer", "Web Developer"],
     typeSpeed : 10,
     backSpeed : 50,
     backDelay : 1000,
